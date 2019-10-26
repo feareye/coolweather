0f318744923e6ef611584a72f94ee52b6074795d
feareye <feareye@feareye.com>
2019-10-26 10:40:29 +0800

    First commit

--- app/src/main/java/com/example/coolweather/MainActivity.java
+++ app/src/main/java/com/example/coolweather/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //和风 key: 3d2c96326ced4589b46d9986056502f6
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
